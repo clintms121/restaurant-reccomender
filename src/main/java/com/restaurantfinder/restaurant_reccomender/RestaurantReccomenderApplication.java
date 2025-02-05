48d6460c833704e3a74f38b25365cea09f347ab4
Clint Stapleton <clintmstapleton102@gmail.com>
2025-02-05 15:46:54 -0600

    Initial commit

---
+++ src/main/java/com/restaurantfinder/restaurant_reccomender/RestaurantReccomenderApplication.java
@@ -0,0 +1,15 @@
+package com.restaurantfinder.restaurant_reccomender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReccomenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReccomenderApplication.class, args);
+
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/restaurantfinder/restaurant_reccomender/RestaurantReccomenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantfinder.restaurant_reccomender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReccomenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
